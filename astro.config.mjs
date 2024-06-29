2719c4212ab1b281a2cb984847127900dd454ab9
Sridhar Ananthakrishnan <2061018+sridharkrishna@users.noreply.github.com>
2024-06-29 19:00:33 +0000

    base path modified to root

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://kriyaetive.github.io',
-	base: '/page',
 	integrations: [mdx(), sitemap()],
 });
