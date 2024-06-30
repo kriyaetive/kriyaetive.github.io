8b6963839c4d3ed88af85c4847826020d30c0655
Sridhar Ananthakrishnan <2061018+sridharkrishna@users.noreply.github.com>
2024-06-30 14:19:21 +0000

    layout update for pages

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,32 @@
 
 export const SITE_TITLE = "Kriyaetive :: Inspiring Creations, Elevating Minds!";
 export const SITE_DESCRIPTION = "Welcome to Kriyaetive";
+
+// Navigation Menus
+export const NAV_MENUS = [
+  {
+    title: "home",
+    tabIndex: 0,
+    href: "/",
+  },
+  {
+    title: "what are we doing?",
+    tabIndex: 0,
+    href: "/whatwedo",
+  },
+  {
+    title: "about us",
+    tabIndex: 0,
+    href: "/about",
+  },
+  {
+    title: "blog",
+    tabIndex: 0,
+    href: "/blog",
+  },
+  {
+    title: "contact us",
+    tabIndex: 0,
+    href: "/contact",
+  },
+];
